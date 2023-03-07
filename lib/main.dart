50dd6024f489bbe648a5ea9f056a270e6d8f27e2
YogeshRajgure <noobcoderyogesh.vraj@gmail.com>
2023-03-07 17:41:04 +0530

    created widget transaction_list

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:intl/intl.dart';
-
 import 'package:flutter/material.dart';
 
-import './models/transaction.dart';
+import './widgets/transaction_list.dart';
 
 void main() {
   runApp(MyApp());
@@ -20,128 +19,66 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  final List<Transaction> transactions = [
-    Transaction(
-      id: 't1',
-      title: 'New Shoes',
-      amount: 2000,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'cookies',
-      amount: 400,
-      date: DateTime.now(),
-    )
-  ];
   // String? titleInput;
   // String? amountInput;
   final titlController = TextEditingController();
   final amountController = TextEditingController();
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Flutter App 2'),
-        ),
-        body: Column(
-          // mainAxisAlignment: MainAxisAlignment.spaceAround,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Container(
-              width: double.infinity,
-              child: Card(
-                color: Colors.blue,
-                elevation: 6,
-                child: Text('CHART!'),
+      appBar: AppBar(
+        title: const Text('Flutter App 2'),
+      ),
+      body: Column(
+        // mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.blue,
+              elevation: 6,
+              child: Text('CHART!'),
+            ),
+          ),
+          Card(
+            elevation: 5,
+            child: Container(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: <Widget>[
+                  TextField(
+                    decoration: InputDecoration(labelText: 'Title'),
+                    controller: titlController,
+                    // onChanged: (val) {
+                    //   titleInput = val;
+                    // },
+                  ),
+                  TextField(
+                    decoration: InputDecoration(labelText: 'Amount'),
+                    controller: amountController,
+                    // onChanged: (val) {
+                    //   amountInput = val;
+                    // },
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      print(titlController.text);
+                    },
+                    style: TextButton.styleFrom(
+                      foregroundColor: Colors.purple,
+                    ),
+                    child: const Text('Add Transaction'),
+                  )
+                ],
               ),
             ),
-            Card(
-              elevation: 5,
-              child: Container(
-                padding: EdgeInsets.all(10),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: <Widget>[
-                    TextField(
-                      decoration: InputDecoration(labelText: 'Title'),
-                      controller: titlController,
-                      // onChanged: (val) {
-                      //   titleInput = val;
-                      // },
-                    ),
-                    TextField(
-                      decoration: InputDecoration(labelText: 'Amount'),
-                      controller: amountController,
-                      // onChanged: (val) {
-                      //   amountInput = val;
-                      // },
-                    ),
-                    TextButton(
-                      onPressed: () {
-                        print(titlController.text);
-                      },
-                      style: TextButton.styleFrom(
-                        foregroundColor: Colors.purple,
-                      ),
-                      child: const Text('Add Transaction'),
-                    )
-                  ],
-                ),
-              ),
-            ),
-            Column(
-                children: transactions.map((tx) {
-              return Card(
-                child: Row(
-                  children: <Widget>[
-                    Container(
-                      margin: EdgeInsets.symmetric(
-                        vertical: 10,
-                        horizontal: 15,
-                      ),
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.purple,
-                          width: 2,
-                        ),
-                      ),
-                      padding: EdgeInsets.all(10),
-                      child: Text(
-                        '\₹ ${tx.amount}', //tx.amount.toString(),
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                          color: Colors.purple,
-                        ),
-                      ),
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          tx.title as String,
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        Text(
-                          DateFormat('yyyy/MM/dd').format(
-                              tx.date as DateTime), //tx.date.toString(),
-                          style: TextStyle(
-                            color: Colors.grey,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              );
-            }).toList()),
-          ],
-        ));
+          ),
+          TransactionList()
+        ],
+      ),
+    );
   }
 }
